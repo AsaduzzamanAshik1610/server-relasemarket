cafaf39ce045a4e25a5e441711722a106e405e93
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-11-25 18:18:40 +0600

    added the port 5000

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('Hellow seller server side')
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening to port ${port}`);
+})
